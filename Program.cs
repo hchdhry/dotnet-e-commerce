f10aa2ddca2073397acc9d122b1fa03c9f6961a1
hassan <hchoudhury20@hotmail.com>
2023-12-18 19:57:24 +0000

    create database for categories

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using MVC_ecom.data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options=>options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ data/appDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MVC_ecom.Model;
+
+namespace MVC_ecom.data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options){
+
+        }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
