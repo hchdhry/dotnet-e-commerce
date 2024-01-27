849a0f7b053ed7dc2cda60ecaf71c729aefc67ce
hassan <hchoudhury20@hotmail.com>
2024-01-27 16:47:39 +0000

    add home and details page

--- MVC-ecom/Areas/Customer/Controller/HomeController.cs
+++ MVC-ecom/Areas/Customer/Controller/HomeController.cs
@@ -1,21 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MVC.DataAcess.Repository.IRepository;
+using MVC.Model;
 using MVC_ecom.Models;
 
 namespace MVC_ecom.Controllers;
 
 public class HomeController : Controller
 {
+
+    private readonly IUnitOfWork _UnitofWork;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _UnitofWork = unitOfWork;
+
     }
 
     public IActionResult Index()
     {
-        return View();
+       IEnumerable<Product> productList = _UnitofWork.product.GetAll(includeProperties: "Category");
+        
+        return View(productList);
+    }
+    public IActionResult Details(int ? productId)
+    {
+        Product Product = _UnitofWork.product.get(u=>u.Id == productId,includeProperties: "Category");
+        return View(Product);
     }
 
     public IActionResult Privacy()
